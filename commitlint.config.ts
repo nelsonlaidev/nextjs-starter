19e612705fca0c048a5a94ae9e5e93ef74ea5673
tszhong0411 <me@honghong.me>
2025-04-06 19:08:27 +0800

    feat: use czg instead of cz-git + commitizen

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,5 @@
-import type { UserConfig } from 'cz-git'
+import { defineConfig } from 'cz-git'
 
-const config: UserConfig = {
+export default defineConfig({
   extends: ['@commitlint/config-conventional']
-}
-
-export default config
+})
