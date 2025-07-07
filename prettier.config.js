98e3d4dff085e04db7e0d7c99f5cb4367605f83b
tszhong0411 <me@nelsonlai.me>
2025-07-07 21:59:23 +0800

    chore: remove prettier config package

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,14 @@
-import tszhong0411 from '@tszhong0411/prettier-config'
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: 'none',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
+  printWidth: 100
+}
 
-export default tszhong0411()
+export default config
