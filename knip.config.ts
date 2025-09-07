847505c250fa3831a0a37c722b1849c5297f4cff
Nelson Lai <me@nelsonlai.dev>
2025-09-07 15:18:10 +0800

    chore: no need to install postcss manually

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,7 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
+  ignoreDependencies: ['postcss'],
   compilers: {
     css: (text: string) => [...text.matchAll(/(?<=@)(?:import|plugin)[^;]+/g)].join('\n').replace('plugin', 'import')
   }
